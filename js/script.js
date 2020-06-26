4d6129bcc5146c6a3fa6696538cf52a0338d8ed3
DDH <ddhoang1993@gmail.com>
2020-06-26 13:40:19 -0700

    added darkmode functionality

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const darkmodeOptions = {
+    bottom: '585px', // default: '32px'
+    right: '32px', // default: '32px'
+    left: 'unset', // default: 'unset'
+    time: '0.3s', // default: '0.3s'
+    mixColor: '#fff', // default: '#fff'
+    backgroundColor: '#fff',  // default: '#fff'
+    buttonColorDark: '#100f2c',  // default: '#100f2c'
+    buttonColorLight: '#fff', // default: '#fff'
+    saveInCookies: false, // default: true,
+    label: '🌓', // default: ''
+    autoMatchOsTheme: true // default: tru
+}
+
+const darkmode = new Darkmode(darkmodeOptions);
+
+darkmode.showWidget();
