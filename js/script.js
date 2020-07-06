355f7bb7d81e3bdf03014d1cf7476bff599850c3
DDH <ddhoang1993@gmail.com>
2020-07-06 13:06:20 -0700

    darkmode

--- js/script.js
+++ js/script.js
@@ -3,13 +3,13 @@ const darkmodeOptions = {
     right: '32px', // default: '32px'
     left: 'unset', // default: 'unset'
     time: '0.3s', // default: '0.3s'
-    mixColor: '#fff', // default: '#fff'
+    mixColor: '#f1f1f1', // default: '#fff'
     backgroundColor: '#fff',  // default: '#fff'
     buttonColorDark: '#100f2c',  // default: '#100f2c'
     buttonColorLight: '#fff', // default: '#fff'
     saveInCookies: false, // default: true,
-    label: '🌓', // default: ''
-    autoMatchOsTheme: true // default: tru
+    label: '', // default: ''
+    autoMatchOsTheme: false // default: true
 }
 
 const darkmode = new Darkmode(darkmodeOptions);
